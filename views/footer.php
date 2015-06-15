b77d3e65491201a053e7fd699e4de9480624249f
Rickkorteman <rickkorteman@hotmail.com>
2015-06-15 13:35:32 +0200

    footer

--- views/footer.php
+++ views/footer.php
@@ -5,7 +5,7 @@
     <script src="../js/Mooth.min.js"></script>
     <script src="../js/main.js"></script>
     <div class="footer">
-      <img src="../images/linkedin-icon.png" />
+      <a href="https://www.linkedin.com/profile/view?id=307773804&trk=nav_responsive_tab_profile_pic" target="_blank"><img src="../images/linkedin-icon.png" /></a>
     </div>
   </body>
 </html>
